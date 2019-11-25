7cdf314afb9168122e01df185fdee8197091640f
jayjscotto <jayjscotto@gmail.com>
2019-11-25 18:04:19 -0500

    editing connection for deployment

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,17 @@
 // Set up MySQL connection.
 const mysql = require("mysql");
 
-const connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "root",
-  database: "eatburgers"
-});
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  const connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "root",
+    database: "eatburgers"
+  });
+}
 
 // Make connection.
 connection.connect(function(err) {
